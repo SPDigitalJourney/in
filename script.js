476156c97c7d0046541c4f084e6a9fb1d12bb289
SPDigitalJourney <rpsanjaypaul@gmail.com>
2026-05-24 16:47:00 +0530

    Add initial script.js for DOMContentLoaded event

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Reflex Fitness website loaded!');
+
+    // You can add interactive JavaScript here later
+    // For example, a "scroll to top" button, a simple carousel, form validation, etc.
+});
