8a48a9632e5e955c1f605fca37d825b33e35d1c2
SPDigitalJourney <rpsanjaypaul@gmail.com>
2026-05-26 01:41:14 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Reflex Fitness website loaded!');
-
-    // You can add interactive JavaScript here later
-    // For example, a "scroll to top" button, a simple carousel, form validation, etc.
-});
